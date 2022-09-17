b58bcbbb71d01f3041dd6cb08944a9e6a9b0ec5f
mash-mixture <asadorasoo32@gmail.com>
2022-09-17 21:56:10 +0500

    asad

--- Js chapters/Objects/OOP-basic.js
+++ Js chapters/Objects/OOP-basic.js
@@ -1,2 +1,10 @@
 //***OBJECT ORIENTED PROGRAMING -- CRASH For Beginners */
 console.log('OOP Run...');
+
+class Book{
+    constructor(title , author , date){
+        this.title = title;
+        this.author = author;
+        this.publishDate = date;
+    }
+}
